68c5c7e84891ee7e1aede8b78f462409292d1aee
Alberto Ferreira da Silva <alberto.abfs@hotmail.com>
2021-03-02 12:54:09 -0300

    Cabô aula 1

---
+++ refresh/3Play-async.js
@@ -0,0 +1,24 @@
+const fetchData = () => {
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('DONE');
+        }, 1500);
+    });
+    return promise;
+};
+
+setTimeout(() => {
+    console.log('Timer is DONE!');
+    
+    fetchData()
+    .then(text => {
+        console.log(text);
+        return fetchData();
+    })
+    .then(text2 => {
+        console.log(text2);
+    });
+
+}, 2000);
+
+console.log('loading...');
